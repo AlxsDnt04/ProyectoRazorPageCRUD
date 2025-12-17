34c04b7f7ef29dfe8d4a516f326c631d7cfdf3c1
Alexis <carapedoxd@gmail.com>
2025-12-16 21:17:54 -0500

    Actualización para modelos, estilos en Pages y la pantalla principal usando <link>

--- ProyectoRazor/Models/Course.cs
+++ ProyectoRazor/Models/Course.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoRazor.Models
 {
     public class Course
     {
         public int Id { get; set; }
+        [Display(Name = "Título")]
         public string Title { get; set; }
+        [Display(Name = "Créditos")]
         public int Credits { get; set; }
 
         // Relación 1 a muchos
--- ProyectoRazor/Models/Enrollment.cs
+++ ProyectoRazor/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace ProyectoRazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoRazor.Models
 {
     public class Enrollment
     {
@@ -6,12 +8,14 @@
 
         // FK hacia Student
         public int StudentId { get; set; }
+        [Display(Name = "Estudiante")]
         public Student Student { get; set; }
 
         // FK hacia Course
         public int CourseId { get; set; }
+        [Display(Name = "Curso")]
         public Course Course { get; set; }
-
+        [Display(Name = "Calificación")]
         public string Grade { get; set; }
     }
 }
